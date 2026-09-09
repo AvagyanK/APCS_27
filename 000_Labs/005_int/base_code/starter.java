86ab40d996fae64a61e9189647bfa5c160508b82
AvagyanK <karavagyan66@gmail.com>
2026-09-09 19:09:56 +0000

    update

--- 000_Labs/005_int/base_code/starter.java
+++ 000_Labs/005_int/base_code/starter.java
@@ -9,7 +9,14 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		int joe;
-		System.out.print(joe); 
-	}
+		
+		int two = 2;
+		int four = 4;
+		int five = 5;
+		int seven = 7;
+		int p1;
+		p1 = five*7/two%3-1;
+		System.out.println(p1);
+
+}
 }
