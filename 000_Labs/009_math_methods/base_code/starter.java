27b928a55678c3f53640996dd6e32958b510a784
AvagyanK <karavagyan66@gmail.com>
2026-09-17 18:58:59 +0000

    update

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -1,15 +1,38 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Karen Avagyan
+ *  Date: 9/16
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		int x = Math.min(5-7*3,8%5*7);
-		System.out.println(x);
+		
+		// int x = Math.min(5-7*3,8%5*7);
+		// System.out.println(x);
+
+		System.out.println(Math.max(13 - 6 * 11, 30 % 7 * (-2)));
+		System.out.println(Math.sqrt(3*8 + 31%7));
+		System.out.println(Math.pow(37/3, 35%21));
+		System.out.println(Math.max(Math.pow(2,14%3), Math.sqrt(2*6)));
+
+///////////////////Extra Lab
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter a double: ");
+		double x = sc.nextDouble();
+
+		System.out.println("Please enter another double: ");
+		double y = sc.nextDouble();
+
+		double max = Math.max(x,y);
+		System.out.println("Maximum number of x and y is: "+max);
+
+		double sqrt = Math.sqrt(y);
+		System.out.println("Square root of y is: "+sqrt);
+
+		double power = Math.pow(x, y);
+		System.out.println("Power of x and y is: "+power);
 	}
 }
--- 102_Exercises/09_Restaurant/basecode/starter.java
+++ 102_Exercises/09_Restaurant/basecode/starter.java
@@ -1,13 +1,79 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Karen Avagyan
+ *  Date: 9/14
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// Your code goes below here
 		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("What is the Restaurant's name?");
+		String res = sc.nextLine();
+
+		System.out.println("What is your name?");
+		String name = sc.nextLine();
+		
+		System.out.println("Pick an item!");
+		String item = sc.nextLine();
+
+		System.out.println("How much is it?");
+		double price = sc.nextDouble();
+
+		System.out.println("How many do you need?");
+		int qty = sc.nextInt();
+		sc.nextLine();
+
+
+		System.out.println("Pick a drink!");
+		String drink = sc.nextLine();
+
+		System.out.println("How much is it?");
+		double dprice = sc.nextDouble();
+
+		System.out.println("How many do you need?");
+		int dqty = sc.nextInt();
+		sc.nextLine();
+
+
+		System.out.println("Anything else?");
+		String any = sc.nextLine();
+
+		System.out.println("How much is it?");
+		double aprice = sc.nextDouble();
+
+		System.out.println("How many do you need?");
+		int aqty = sc.nextInt();
+
+
+		System.out.println("What tip percentage would you like to leave?");
+		double tip = sc.nextDouble();
+
+		double subtotal = qty*price + dqty*dprice + aqty*aprice;
+		double tax = subtotal / 9.75;
+		double tip_am = subtotal * tip / 100;
+		double total = subtotal + tax + tip_am;
+
+		System.out.println("========================================");
+		System.out.println("    " + res + " - Menu For Today");
+		System.out.println("========================================");
+		System.out.println("Owner: " + name);
+		System.out.println("----------------------------------------");
+		System.out.println("Item         Qty     Price");
+		System.out.println("----------------------------------------");
+		System.out.println(item + "        " + qty + "       " + price);
+		System.out.println(drink + "          " + dqty + "       " + dprice);
+		System.out.println(any + "         " + aqty + "       " + aprice);
+		System.out.println("----------------------------------------");
+		System.out.println("Subtotal:             " + subtotal);
+		System.out.println("Tax (9.75%):" + "          " +  tax);
+		System.out.println("Tip:                  " + tip);
+		System.out.println("Tip Amount:           " + tip_am);
+		
+		System.out.println("========================================");
+		System.out.println("TOTAL:                " + total);
+		System.out.println("========================================");
 	}
 }
